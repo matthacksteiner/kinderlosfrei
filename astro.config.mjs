d66d43216ce22826939987e89d426c4e4b539ecc
Matthias Hacksteiner <contact@matthiashacksteiner.net>
2023-02-22 15:29:37 +0100

    astro setup

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	// site: 'https://physioflatz.at/',
+	integrations: [
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		image({
+			serviceEntryPoint: '@astrojs/image/sharp',
+		}),
+		sitemap(),
+	],
+});
