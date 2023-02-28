f455fbebc6f1b21190190e0d0761ae4a9f35d8be
Matthias Hacksteiner <contact@matthiashacksteiner.net>
2023-02-28 11:09:31 +0100

    logo width

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -2,8 +2,8 @@ var header = document.querySelector('header');
 
 window.onscroll = function () {
 	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-		header.classList.add('header--small');
+		header.classList.add('header--active');
 	} else {
-		header.classList.remove('header--small');
+		header.classList.remove('header--active');
 	}
 };
