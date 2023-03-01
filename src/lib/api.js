dbf6d222e89b5c883c3f7d2c436e01e9d5cb3e50
Matthias Hacksteiner <contact@matthiashacksteiner.net>
2023-03-01 18:22:38 +0100

    font wip

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,6 +3,9 @@ const API_URL = import.meta.env.KIRBY_URL;
 export async function getIndex(uri) {
 	const response = await fetch(API_URL + uri, {
 		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
 	});
 	if (response.status !== 200) {
 		console.log(API_URL);
