74abed1ffda025e9a960d2c791dcc8ebce4144af
Matthias Hacksteiner <contact@matthiashacksteiner.net>
2023-03-10 08:40:43 +0100

    get font external file

--- src/lib/api.js
+++ src/lib/api.js
@@ -13,3 +13,46 @@ export async function getIndex(uri) {
 
 	return json;
 }
+
+// export the getFonts function
+export async function getFonts() {
+	// call getIndex with some uri
+	const data = await getIndex('/home.json');
+	const global = data.global;
+	// use index to get global.font or do something else
+	return `${global.font
+		.map((item) => {
+			return `@font-face {
+		font-family: '${item.name}';
+		src: url('${item.url2}') format('woff2'),
+			 url('${item.url1}') format('woff');
+		font-weight: normal;
+		font-style: normal;
+		font-display: swap;
+	  }`;
+		})
+		.join('')}`;
+}
+
+// export all font sizes
+export async function getSizes() {
+	const baseFontSize = 16;
+	const data = await getIndex('/home.json');
+	const global = data.global;
+	return `${global.fontSize
+		.map((item) => {
+			return `
+		.font--${item.name} {
+			font-size: ${item.sizeMobile / baseFontSize}rem;
+			line-height: ${item.lineHeightMobile / item.sizeMobile};
+		}
+		@media (min-width: 768px) {
+			.font--${item.name} {
+		font-size: ${item.sizeDesktop / baseFontSize}rem;
+		line-height: ${item.lineHeightDesktop / item.sizeDesktop};
+		}
+	}
+      `;
+		})
+		.join('')}`;
+}
