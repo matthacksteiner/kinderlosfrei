bd6d36a535c5ddf18205df2e4927da2babde8bbe
Matthias Hacksteiner <contact@matthiashacksteiner.net>
2023-04-25 16:33:29 +0200

    hamburger center

--- src/scripts/header.js
+++
@@ -1,9 +0,0 @@
-var header = document.querySelector('header');
-
-window.onscroll = function () {
-	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-		header.classList.add('header--active');
-	} else {
-		header.classList.remove('header--active');
-	}
-};
