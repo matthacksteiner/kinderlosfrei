e99f349ce9c9e12fc68333dda36ee5a75dfa3f8d
Matthias Hacksteiner <contact@matthiashacksteiner.net>
2023-12-01 09:05:58 +0100

    Fix frontend URL formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import { defineConfig } from 'astro/config';
 const API_URL = process.env.KIRBY_URL;
 const response = await fetch(API_URL + '/global.json');
 const data = await response.json();
-const frontendUrl = data.frontendUrl;
+const frontendUrl = data.frontendUrl.endsWith('/')
+	? data.frontendUrl
+	: data.frontendUrl + '/';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
