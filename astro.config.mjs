11d5f5fa9bc689305e9051ec8b7df5fa7b440fb6
Matthias Hacksteiner <contact@matthiashacksteiner.net>
2024-02-07 15:06:55 +0100

    Update i18n configuration and remove redirect code

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,12 @@ import { defineConfig } from 'astro/config';
 const API_URL = process.env.KIRBY_URL;
 const response = await fetch(API_URL + '/global.json');
 const data = await response.json();
+const defaultLanguage = data.defaultLang.code;
+const defaultLanguageLocale = data.defaultLang.locale.replace(/_/g, '-');
+const translations = data.translations.map((lang) => lang.code);
+const translationsLocales = data.translations.map((lang) =>
+	lang.locale.replace(/_/g, '-')
+);
 const frontendUrl = data.frontendUrl.endsWith('/')
 	? data.frontendUrl
 	: data.frontendUrl + '/';
@@ -14,7 +20,28 @@ import netlify from '@astrojs/netlify/functions';
 // https://astro.build/config
 export default defineConfig({
 	site: frontendUrl,
-	integrations: [tailwind(), sitemap()],
+	i18n: {
+		defaultLocale: defaultLanguage,
+		locales: [defaultLanguage, ...translations],
+		routing: {
+			prefixDefaultLocale: true,
+		},
+	},
+	integrations: [
+		tailwind(),
+		sitemap({
+			i18n: {
+				defaultLocale: defaultLanguage,
+				locales: {
+					[defaultLanguage]: defaultLanguageLocale,
+					...translations.reduce((acc, lang, index) => {
+						acc[lang] = translationsLocales[index];
+						return acc;
+					}, {}),
+				},
+			},
+		}),
+	],
 	image: {
 		domains: [API_URL],
 	},
