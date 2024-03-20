def5ceaad9452368b5b4eda6164c4a9a437abf8c
Matthias Hacksteiner <contact@matthiashacksteiner.net>
2024-03-20 08:48:54 +0100

    Update sitemap link and fetch global data in robots.txt.ts

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,14 +1,31 @@
 import type { APIRoute } from 'astro';
 
-const robotsTxt = `
+interface GlobalData {
+	frontendUrl: string;
+}
+
+const getGlobalData = async (): Promise<GlobalData> => {
+	const API_URL = import.meta.env.KIRBY_URL;
+	const response = await fetch(API_URL + '/global.json');
+	const data = await response.json();
+	return data;
+};
+
+const robotsTxt = async (frontendUrl: string) =>
+	`
 User-agent: *
 Allow: /
 
-Sitemap: ${new URL('sitemap.xml', import.meta.env.KIRBY_URL).href}
+Sitemap: ${new URL('sitemap-index.xml', frontendUrl).href}
 `.trim();
 
-export const GET: APIRoute = () => {
-	return new Response(robotsTxt, {
+export const GET: APIRoute = async () => {
+	const globalData = await getGlobalData();
+	const frontendUrl = globalData.frontendUrl.endsWith('/')
+		? globalData.frontendUrl
+		: globalData.frontendUrl + '/';
+
+	return new Response(await robotsTxt(frontendUrl), {
 		headers: {
 			'Content-Type': 'text/plain; charset=utf-8',
 		},
