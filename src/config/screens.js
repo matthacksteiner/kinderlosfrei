8ef8c54bb8dd325499a4a2fb3dff020caa56d5c4
Matthias Hacksteiner <contact@matthiashacksteiner.net>
2025-03-25 16:25:26 +0100

    first Take

---
+++ src/config/screens.js
@@ -0,0 +1,9 @@
+// src/config/screens.js
+export const screens = {
+	sm: '640px',
+	md: '768px',
+	lg: '1024px',
+	xl: '1280px',
+	'2xl': '1440px',
+	'3xl': '1920px',
+};
--- src/config/tailwind.baseConfig.cjs
+++ src/config/tailwind.baseConfig.cjs
@@ -58,14 +58,6 @@ module.exports = {
 		'w-full',
 	],
 	theme: {
-		screens: {
-			sm: '640px',
-			md: '768px',
-			lg: '1024px',
-			xl: '1280px',
-			'2xl': '1440px',
-			'3xl': '1920px',
-		},
 		container: {
 			padding: {
 				DEFAULT: 'var(--grid-margin-mobile)',
